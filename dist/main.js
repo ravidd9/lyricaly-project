323a955c438bd398a4d3a1d480eaae9e2d130e42
Ravid Cohen <ravidd9@gmail.com>
2019-04-17 11:30:39 +0300

    ravid3

--- dist/main.js
+++ dist/main.js
@@ -2,12 +2,15 @@ let apiManager = new ApiManager()
 let renderer = new Renderer()
 
       
+const loadPage = async function(){
+    await apiManager.getDataFromDB()
+    renderer.render(apiManager)
+}
 
 const querySearch = async function(){
     let query = $("#querySearch").find("input").val()
     if(query != ""){
         await apiManager.getSongID(query)
-        console.log(apiManager.songData)
         renderer.render(apiManager)
     }
 }
@@ -15,14 +18,12 @@ const querySearch = async function(){
 const querySave = async function(){
     let id = $(this).closest(".songCard").attr('id')
     apiManager.saveSong(id)
-    
 }
 
 const queryDelete = async function(){
     let id = $(this).closest(".songCard").attr('id')
     console.log(id)
-    console.log("wow")
-
     apiManager.removeSong(id)
-    
 }
+
+loadPage()
