c2722983cd12be543ec98255d3594eeb552c691d
Ravid Cohen <ravidd9@gmail.com>
2019-04-17 12:30:12 +0300

    ravid3

--- dist/ApiManager.js
+++ dist/ApiManager.js
@@ -1,6 +1,18 @@
 class ApiManager {
     constructor() {
-        this.songData = {}
+        this.songData = {
+            name: "Chandelier",
+            id: 378195,
+            artist: "Sia",
+            artistID: 16775,
+            titleFeatured: "Chandelier",
+            album: "1000 Forms of Fear",
+            albumID: 104614,
+            lyrics: "adad",
+            songPic: "https://images.genius.com/5bcfb76690b3fb068a317c76579b70b5.1000x1000x1.jpg",
+            applePlayer: "https://genius.com/songs/378195/apple_music_player",
+            youTubePlayer: "http://www.youtube.com/watch?v=2vjPBrBU-TM"
+        }
         this.favorites = []
         this.others = []
         this.ids = []
